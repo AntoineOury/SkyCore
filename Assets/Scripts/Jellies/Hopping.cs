32dff56b0963f9c565b2d1bac13d29fb089acdab
Megafont <51346737+Megafont@users.noreply.github.com>
2024-04-05 13:10:14 -0500

    Improved Jelly movement
    + Hopping script has min/max jump height settings so their movement feels a bit more natural now.
    + Added min/max delay settings to Hopping.cs so they don't always hop at the exact same interval.
    + Jelly hopping is now using the rigidbody, though it disables the navmeshAgent temporarily while hopping (otherwise he just stays glued to the ground when he should be hopping)
    + Added a simple face on the jelly so its easier to see which way he's facing

--- Assets/Scripts/Jellies/Hopping.cs
+++ Assets/Scripts/Jellies/Hopping.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Hopping : MonoBehaviour
 {
@@ -9,18 +10,41 @@ public class Hopping : MonoBehaviour
     [SerializeField, Tooltip("We manipulate local position of the model itself so we need a reference to just the model")]
     private GameObject _model;
 
-    [SerializeField, Tooltip("The amount of time taken for jump to cpmplete")]
-    private float _durationOfJump;
 
-    [SerializeField, Tooltip("Total Height in Unity units for the Jelly to hop")]
-    private float _totalJumpHeight;
+    [Header("Hopping Parammeters")]
+
+    [SerializeField, Min(0), Tooltip("Min height in Unity units of the Jelly's hop.")]
+    private float _minHopHeight = 0.30f;
+
+    [SerializeField, Min(0), Tooltip("Max height in Unity units of the Jelly's hop.")]
+    private float _maxHopHeight = 0.76f;
+
+    [SerializeField, Min(0), Tooltip("Max time (in seconds) between the Jelly's hops.")]
+    private float _minTimeBetweenHops = 0.5f;
+
+    [SerializeField, Min(0), Tooltip("Min height in Unity units of the Jelly's hop.")]
+    private float _maxTimeBetweenHops = 1f;
+
 
     [SerializeField]
     private JellyStateMachine _stateMachine;
     [SerializeField]
+    private GameEventScriptableObject _wanderEnter;
+    [SerializeField]
+    private GameEventScriptableObject _wanderUpdate;
+    [SerializeField]
     private GameEventScriptableObject _wanderExit;
 
-    private bool _alreadyHopping;
+
+    private NavMeshAgent _navAgent;
+    private Rigidbody _rigidbody;
+
+    private bool _isGrounded = true;
+
+    private float _hopTimer;
+    private float _groundedTimer;
+
+    private bool _hopRoutineIsRunning;
 
     private bool _isFinished = true;
 
@@ -28,46 +52,116 @@ public class Hopping : MonoBehaviour
 
     private void Awake()
     {
-        _wandering.OnChangeDirection += SetIsFinished;
-        _gameEventResponses.SetSelectiveResponses(_stateMachine
-            , (_wanderExit, SetIsFinished));
+        //_wandering.OnChangeDirection += SetIsFinished;
+        _gameEventResponses.SetSelectiveResponses(_stateMachine,
+            (_wanderEnter, WanderEnter),
+            (_wanderUpdate, WanderUpdate),
+            (_wanderExit, WanderExit));
+
+        _navAgent = GetComponent<NavMeshAgent>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void OnEnable() => _gameEventResponses.Register();
     private void OnDisable() => _gameEventResponses.Unregister();
 
-    void Update()
+    public void WanderEnter()
     {
-        if (!_alreadyHopping && !_isFinished)
+        _isFinished = false;
+    }
+
+    public void WanderUpdate()
+    {
+        CheckIfGrounded();
+
+        _hopTimer -= Time.deltaTime;
+        if (!_hopRoutineIsRunning && _isGrounded && _hopTimer <= 0 && !_isFinished)
         {
-            StartCoroutine(PerformTheHop());            
+            StartCoroutine(StartHop());
+        }
+    }
+
+    public void WanderExit()
+    {
+        _isFinished = true;
+    }
+
+    private void CheckIfGrounded()
+    {
+        // If the jelly's vertical velocity is near zero, increment the groundedTimer.
+        if (_rigidbody.velocity.y > -0.1f && _rigidbody.velocity.y < 0.1f)
+        {
+            _groundedTimer += Time.deltaTime;
+        }
+        // If the jelly's vertical velocity is moving upward, then reset the grounded timer to 0f.
+        else if (_rigidbody.velocity.y >= 0.1f)
+        {
+            _isGrounded = false;
+            _groundedTimer = 0f;
+        }
+        
+        // If the jelly has been on the ground a short time, then set _isGrounded to true.
+        if (_groundedTimer >= 0.3f)
+        {
+            _isGrounded = true;
         }
     }
 
     private void SetIsFinished(bool newValue)
     {
-        _isFinished = newValue;
+        Debug.Log("Set: " +  newValue);
+        //_isFinished = newValue;
     }
 
-    private void SetIsFinished()
+    private IEnumerator StartHop()
     {
-        _isFinished = true;
-    }
+        _hopRoutineIsRunning = true;
 
-    private IEnumerator PerformTheHop()
-    {
-        _alreadyHopping = true;
-        Transform localTransform = _model.transform;
-        float trueHeight = _totalJumpHeight / Mathf.Pow(_durationOfJump / 2, 2); 
+        _rigidbody.isKinematic = false;
 
-        for (float i = 0; i < _durationOfJump; i += Time.deltaTime)
+        _navAgent.enabled = false;
+        _rigidbody.velocity += GetJumpVelocity();
+        _isGrounded = false;
+
+
+        // We put a quarter second on the timer, as this is being used to disable the code in OnCollisionEnter that detects when the jelly is back on the ground.
+        // This short delay is needed, as otherwise the OnCollisionEnter() function will instantly set the _isGrounded variable to true again before the jelly
+        // has a chance to jump off the ground.
+        _hopTimer = 0.1f;
+        while (!_isGrounded)
         {
-            Vector3 localPos = localTransform.localPosition;
-            localTransform.localPosition = new Vector3(localPos.x, i * trueHeight * (_durationOfJump - i), localPos.z);
             yield return null;
         }
 
-        _alreadyHopping = false;
+
+        // Reset the hop timer, as it will now be used to handle the delay between hops.
+        _hopTimer = Random.Range(_minTimeBetweenHops, _maxTimeBetweenHops);
+
+        _navAgent.enabled = true;
+        _rigidbody.isKinematic = true;
+
+        _hopRoutineIsRunning = false;
+    }
+
+
+    private Vector3 GetJumpVelocity()
+    {
+        // This formula (u*u = 2gS) is calculating the velocity needed to reach the specified jump height.        
+        // I derived it from this page at #2 under "Maximum height of a projectile": https://byjus.com/question-answer/how-do-you-determine-the-maximum-height-of-a-projectile/
+        // NOTE: I removed v squared and added (2gH) to both sides so u squared is by itself so we can now solve for it.
+        //       v squared is the final velocity, which is 0 since the jelly lands at the end of the jump.
+        //       u squared is the initial velocity we're solving for.
+        //       g is gravity
+        //       S is the distance traveled, which is our total jump height.
+        float minJumpVelocity = Mathf.Sqrt(2 * -(Physics.gravity.y) * _minHopHeight);
+        float maxJumpVelocity = Mathf.Sqrt(2 * -(Physics.gravity.y) * _maxHopHeight);
+
+        Vector3 jumpVelocity = transform.forward * _navAgent.speed;
+        jumpVelocity.y = Random.Range(minJumpVelocity, maxJumpVelocity);
+
+        //Debug.Log(jumpVelocity);
+
+        return jumpVelocity;
     }
 
 }
